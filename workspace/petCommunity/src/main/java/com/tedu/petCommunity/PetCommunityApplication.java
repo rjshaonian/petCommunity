2f2ad8b9706520a29929477631845f6dc0fa0cff
tedu <1353764427@qq.com>
2019-11-16 17:16:28 +0800

    欢迎顺哥的标语

--- workspace/petCommunity/src/main/java/com/tedu/petCommunity/PetCommunityApplication.java
+++ workspace/petCommunity/src/main/java/com/tedu/petCommunity/PetCommunityApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PetCommunityApplication {
-
 	public static void main(String[] args) {
+
+		SpringApplication.run(PetCommunityApplication.class, args);
 		// 2019-11-16 阳昊
 		System.out.println("测试一次提交");
-		SpringApplication.run(PetCommunityApplication.class, args);
+		// 2019-11-16 王李顺 
+		System.out.println("欢迎顺哥来到宠物社区");
 	}
 
 }
