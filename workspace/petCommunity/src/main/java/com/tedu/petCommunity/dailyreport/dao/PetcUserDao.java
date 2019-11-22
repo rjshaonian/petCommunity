65aa0edb8f7677dd6445f5a0e201188d4bef6b41
Liam <1353764427@qq.com>
2019-11-22 10:26:13 +0800

    PetcUserDao.java-创建UserDao类

---
+++ workspace/petCommunity/src/main/java/com/tedu/petCommunity/dailyreport/dao/PetcUserDao.java
@@ -0,0 +1,12 @@
+package com.tedu.petCommunity.dailyreport.dao;
+
+import com.tedu.petCommunity.sys.entity.PetcUserPO;
+
+public interface PetcUserDao {
+	/**
+	 * @author Liam-王李顺
+	 * @param userInfo
+	 * @return
+	 */
+	int updateUserInfo(PetcUserPO userInfo);
+}
