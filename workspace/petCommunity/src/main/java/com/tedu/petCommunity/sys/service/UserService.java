4026e7d602c7aafb81ceca0b0f08488f6fa466c7
tedu <1353764427@qq.com>
2019-11-22 15:12:19 +0800

    换了包,加了测试类TestUpdateUserInfo.java

---
+++ workspace/petCommunity/src/main/java/com/tedu/petCommunity/sys/service/UserService.java
@@ -0,0 +1,12 @@
+package com.tedu.petCommunity.sys.service;
+
+import com.tedu.petCommunity.sys.entity.PetcUserPO;
+
+/**
+ * 
+ * @author Liam-顺
+ * @createdTime 创建时间：2019年11月22日 下午3:07:26
+ */
+public interface UserService {
+	int updatePetcUser(PetcUserPO petcUserPO, Integer id);
+}
