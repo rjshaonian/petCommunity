381524f2055a5856a4df0503ac35cbc99dc9094c
Liam-顺 <1353764427@qq.com>
2019-11-22 10:51:57 +0800

    PetcUserDao.java--添加文档修改时间

--- workspace/petCommunity/src/main/java/com/tedu/petCommunity/dailyreport/dao/PetcUserDao.java
+++ workspace/petCommunity/src/main/java/com/tedu/petCommunity/dailyreport/dao/PetcUserDao.java
@@ -4,9 +4,10 @@ import com.tedu.petCommunity.sys.entity.PetcUserPO;
 
 public interface PetcUserDao {
 	/**
-	 * @author Liam-王李顺
+	 * @author Liam-顺
 	 * @param userInfo
 	 * @return
+	 * @createdTime 创建时间：2019年11月22日 上午10:50:39
 	 */
 	int updateUserInfo(PetcUserPO userInfo);
 }
