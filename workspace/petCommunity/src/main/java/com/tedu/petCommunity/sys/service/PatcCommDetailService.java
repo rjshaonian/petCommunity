627574de5b677216cab257076eb502be5ca2b03c
714523613 <714523613@qq.com>
2019-11-23 18:49:18 +0800

    写了两网页,增加两个controller类

---
+++ workspace/petCommunity/src/main/java/com/tedu/petCommunity/sys/service/PatcCommDetailService.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.tedu.petCommunity.sys.service;
+
+import java.util.List;
+
+import com.tedu.petCommunity.sys.vo.PetcCommDetailVO;
+
+/**
+ * @author VictorHe 2019年11月23日 下午4:26:02
+ */
+public interface PatcCommDetailService {
+	static List<PetcCommDetailVO> getCommDetail(String name, Integer user_type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
