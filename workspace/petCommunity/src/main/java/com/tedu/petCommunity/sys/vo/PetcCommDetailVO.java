b10dc39e6d3bd91f2c9e4afb62367ac390ecbf3b
714523613 <714523613@qq.com>
2019-11-25 16:31:10 +0800

    社区聊天初步实现

--- workspace/petCommunity/src/main/java/com/tedu/petCommunity/sys/vo/PetcCommDetailVO.java
+++ workspace/petCommunity/src/main/java/com/tedu/petCommunity/sys/vo/PetcCommDetailVO.java
@@ -3,6 +3,8 @@
  */
 package com.tedu.petCommunity.sys.vo;
 
+import java.util.Date;
+
 import lombok.Data;
 
 /**
@@ -10,6 +12,20 @@ import lombok.Data;
  */
 @Data
 public class PetcCommDetailVO {
-	Integer user_type;
-	String name;
+	// 主键id
+	private Integer id;
+	// 社区名
+	private String commName;
+	// 位置
+	private String position;
+	// 是否启用0:未启用;1:启用
+	private Integer valid;
+	// 创建时间
+	private Date createTime;
+	// 修改时间
+	private Date modifiedTime;
+	// 创建者id
+	private Integer createUser;
+	// 修改者id
+	private Integer modifiedUser;
 }
